dcf1c5209259491448a02ca2e7d439ff344dfcb5
Matheus Neder <matheusneder@gmail.com>
2017-06-30 16:38:57 -0300

    Initial commit

---
+++ Source/Xamarin.iOS.CameraViewController/ApiDefinition.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using AVFoundation;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using Photos;
+using UIKit;
+
+namespace Xamarin.iOS.CameraViewController
+{
+    // @interface CameraView : UIView
+    [BaseType(typeof(UIView), Name = "_TtC20CameraViewController10CameraView")]
+    interface CameraView
+    {
+        // @property (nonatomic) AVCaptureDevicePosition currentPosition;
+        [Export("currentPosition", ArgumentSemantic.Assign)]
+        AVCaptureDevicePosition CurrentPosition { get; set; }
+
+        // -(void)startSession;
+        [Export("startSession")]
+        void StartSession();
+
+        // -(void)stopSession;
+        [Export("stopSession")]
+        void StopSession();
+
+        // -(void)layoutSubviews;
+        [Override]
+        [Export("layoutSubviews")]
+        void LayoutSubviews();
+
+        // -(void)configureFocus;
+        [Export("configureFocus")]
+        void ConfigureFocus();
+
+        // -(void)capturePhotoWithCompletion:(void (^ _Nonnull)(UIImage * _Nullable))completion;
+        [Export("capturePhotoWithCompletion:")]
+        void CapturePhotoWithCompletion(Action<UIImage> completion);
+
+        // -(BOOL)focusCameraToPoint:(CGPoint)toPoint __attribute__((warn_unused_result));
+        [Export("focusCameraToPoint:")]
+        bool FocusCameraToPoint(CGPoint toPoint);
+
+        // -(void)cycleFlash;
+        [Export("cycleFlash")]
+        void CycleFlash();
+
+        // -(void)swapCameraInput;
+        [Export("swapCameraInput")]
+        void SwapCameraInput();
+
+        // -(void)rotatePreview;
+        [Export("rotatePreview")]
+        void RotatePreview();
+
+        // -(instancetype _Nonnull)initWithFrame:(CGRect)frame __attribute__((objc_designated_initializer));
+        [Export("initWithFrame:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(CGRect frame);
+    }
+
+    // @interface CameraViewController : UIViewController
+    [BaseType(typeof(UIViewController), Name = "_TtC20CameraViewController20CameraViewController")]
+    interface CameraViewController
+    {
+        // @property (copy, nonatomic) void (^ _Nullable)(UIImage * _Nullable, PHAsset * _Nullable) onCompletion;
+        [NullAllowed, Export("onCompletion", ArgumentSemantic.Copy)]
+        Action<UIImage, PHAsset> OnCompletion { get; set; }
+
+        // -(instancetype _Nonnull)initWithCroppingEnabled:(BOOL)croppingEnabled allowsLibraryAccess:(BOOL)allowsLibraryAccess completion:(void (^ _Nonnull)(UIImage * _Nullable, PHAsset * _Nullable))completion __attribute__((objc_designated_initializer));
+        [Export("initWithCroppingEnabled:allowsLibraryAccess:completion:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(bool croppingEnabled, bool allowsLibraryAccess, Action<UIImage, PHAsset> completion);
+
+        // @property (readonly, nonatomic) BOOL prefersStatusBarHidden;
+        [Override]
+        [Export("prefersStatusBarHidden")]
+        bool PrefersStatusBarHidden();
+
+        // @property (readonly, nonatomic) UIStatusBarAnimation preferredStatusBarUpdateAnimation;
+        [Override]
+        [Export("preferredStatusBarUpdateAnimation")]
+        UIStatusBarAnimation PreferredStatusBarUpdateAnimation { get; }
+
+        // -(void)loadView;
+        [Override]
+        [Export("loadView")]
+        void LoadView();
+
+        // -(void)updateViewConstraints;
+        [Override]
+        [Export("updateViewConstraints")]
+        void UpdateViewConstraints();
+
+        // -(void)viewDidLoad;
+        [Override]
+        [Export("viewDidLoad")]
+        void ViewDidLoad();
+
+        // -(void)viewWillAppear:(BOOL)animated;
+        [Override]
+        [Export("viewWillAppear:")]
+        void ViewWillAppear(bool animated);
+
+        // -(void)viewDidAppear:(BOOL)animated;
+        [Override]
+        [Export("viewDidAppear:")]
+        void ViewDidAppear(bool animated);
+
+        // -(void)viewWillDisappear:(BOOL)animated;
+        [Override]
+        [Export("viewWillDisappear:")]
+        void ViewWillDisappear(bool animated);
+    }
+
+    // @interface ConfirmViewController : UIViewController <UIScrollViewDelegate>
+    [BaseType(typeof(UIViewController), Name = "_TtC20CameraViewController21ConfirmViewController")]
+    interface ConfirmViewController : IUIScrollViewDelegate
+    {
+        // @property (copy, nonatomic) void (^ _Nullable)(UIImage * _Nullable, PHAsset * _Nullable) onComplete;
+        [NullAllowed, Export("onComplete", ArgumentSemantic.Copy)]
+        Action<UIImage, PHAsset> OnComplete { get; set; }
+
+        // -(instancetype _Nonnull)initWithAsset:(PHAsset * _Nonnull)asset allowsCropping:(BOOL)allowsCropping __attribute__((objc_designated_initializer));
+        [Export("initWithAsset:allowsCropping:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(PHAsset asset, bool allowsCropping);
+
+        // @property (readonly, nonatomic) BOOL prefersStatusBarHidden;
+        [Override]
+        [Export("prefersStatusBarHidden")]
+        bool PrefersStatusBarHidden();
+
+        // @property (readonly, nonatomic) UIStatusBarAnimation preferredStatusBarUpdateAnimation;
+        [Override]
+        [Export("preferredStatusBarUpdateAnimation")]
+        UIStatusBarAnimation PreferredStatusBarUpdateAnimation { get; }
+
+        // -(void)viewDidLoad;
+        [Override]
+        [Export("viewDidLoad")]
+        void ViewDidLoad();
+
+        // -(void)viewWillLayoutSubviews;
+        [Override]
+        [Export("viewWillLayoutSubviews")]
+        void ViewWillLayoutSubviews();
+
+        // -(UIView * _Nullable)viewForZoomingInScrollView:(UIScrollView * _Nonnull)scrollView __attribute__((warn_unused_result));
+        [Export("viewForZoomingInScrollView:")]
+        [return: NullAllowed]
+        UIView ViewForZoomingInScrollView(UIScrollView scrollView);
+
+        // -(void)scrollViewDidZoom:(UIScrollView * _Nonnull)scrollView;
+        [Export("scrollViewDidZoom:")]
+        void ScrollViewDidZoom(UIScrollView scrollView);
+    }
+
+    // @interface PhotoLibraryViewController : UIViewController
+    [BaseType(typeof(UIViewController), Name = "_TtC20CameraViewController26PhotoLibraryViewController")]
+    interface PhotoLibraryViewController
+    {
+        // @property (copy, nonatomic) void (^ _Nullable)(PHAsset * _Nullable) onSelectionComplete;
+        [NullAllowed, Export("onSelectionComplete", ArgumentSemantic.Copy)]
+        Action<PHAsset> OnSelectionComplete { get; set; }
+
+        // -(void)viewDidLoad;
+        [Override]
+        [Export("viewDidLoad")]
+        void ViewDidLoad();
+
+        // -(void)viewWillLayoutSubviews;
+        [Override]
+        [Export("viewWillLayoutSubviews")]
+        void ViewWillLayoutSubviews();
+
+        // @property (readonly, nonatomic) UIStatusBarStyle preferredStatusBarStyle;
+        [Override]
+        [Export("preferredStatusBarStyle")]
+        UIStatusBarStyle PreferredStatusBarStyle();
+
+        // -(void)present:(UIViewController * _Nonnull)inViewController animated:(BOOL)animated;
+        [Export("present:animated:")]
+        void Present(UIViewController inViewController, bool animated);
+
+        // -(void)dismissLibrary;
+        [Export("dismissLibrary")]
+        void DismissLibrary();
+
+        // -(instancetype _Nonnull)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil __attribute__((objc_designated_initializer));
+        [Export("initWithNibName:bundle:")]
+        [DesignatedInitializer]
+        IntPtr Constructor([NullAllowed] string nibNameOrNil, [NullAllowed] NSBundle nibBundleOrNil);
+    }
+}
+
---
+++ Source/Xamarin.iOS.CameraViewController/AvoidXamarinBug56962.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.iOS.CameraViewController
+{
+    // Below definitions exists to avoid this bug: https://bugzilla.xamarin.com/show_bug.cgi?id=56962 (Xamarin.iOS binding - Attempting to JIT compile method 'Class:.ctor ()' while running in aot-only mode.)
+    // This types should never be referenced.
+    internal class FakeCameraView : CameraView
+    {
+        FakeCameraView() : base()
+        {
+
+        }
+    }
+
+    internal class FakeCameraViewController : CameraViewController
+    {
+        FakeCameraViewController() : base()
+        {
+
+        }
+    }
+
+    internal class FakeConfirmViewController : ConfirmViewController
+    {
+        FakeConfirmViewController() : base()
+        {
+
+        }
+    }
+
+    internal class FakePhotoLibraryViewController : PhotoLibraryViewController
+    {
+        FakePhotoLibraryViewController() : base()
+        {
+
+        }
+    }
+}
