1dd20f3409cbf4e292db5a61a59a6ba3d0443d84
Matheus Neder <matheusneder@gmail.com>
2017-07-02 16:20:38 -0300

    Update ViewController.cs

--- Example/ViewController.cs
+++ Example/ViewController.cs
@@ -7,10 +7,10 @@ namespace CameraViewControllerExample
 {
     public partial class ViewController : UIViewController
     {
-		protected ViewController(IntPtr handle) : base(handle)
-		{
+	protected ViewController(IntPtr handle) : base(handle)
+	{
 
-		}
+	}
 
         partial void ChangeImageButton_TouchUpInside(UIButton sender)
         {
